cd0419dd956f3882bf6356022b9b365fcd8b08e2
MamadouThiam572 <inconnuenom1808@gmail.com>
2025-08-29 00:01:07 +0000

    Add debug log for MONGODB_URI in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log('MONGODB_URI in db.js:', process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connecté avec succès !");
   } catch (error) {
